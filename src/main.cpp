4d7056a5935da5ba8dfb30372a66b603c1ebe7f2
magley <60552482+magley@users.noreply.github.com>
2023-02-21 20:37:14 +0100

    chore: Integrate sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
+#include <stdio.h>
+#include "SDL2/SDL.h"
 
-#include "util/json/obj_archive_test.hpp"
-#include "util/string/string2_test.hpp"
-#include "util/fixed/fp_test.hpp"
-#include "util/richtext/richtext_test.hpp"
+void print_err(const char* prefix, const char* msg, const char* suffix) {
+    printf("\033[0;37m%s\033[0;31m%s\033[0;37m%s", prefix, msg, suffix);
+}
+
+void handle_sdl_error() {
+    const char* error = SDL_GetError();
+    print_err("SDL2: ", error, "\n");
+}
 
 int main(int argc, char** argv) {
-    string2_Test string2_test;
-    string2_test.run();
+    int status = SDL_Init(SDL_INIT_VIDEO);
+    if (status != 0) {
+        handle_sdl_error();
+        return 1;
+    }
 
-    ObjArchive_Test obj_archive_test;
-    obj_archive_test.run();
-
-    fp6_Test fp6_test;
-    fp6_test.run();
-
-    richtext_Test richtext_test;
-    richtext_test.run();
-
+    SDL_Quit();
     return 0;
 }
