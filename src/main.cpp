01af8d15a1cc44db6a84f90cda676a3b9e171362
magley <60552482+magley@users.noreply.github.com>
2023-02-21 20:46:04 +0100

    chore: Add SDL2 window init and loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,56 @@ void print_err(const char* prefix, const char* msg, const char* suffix) {
     printf("\033[0;37m%s\033[0;31m%s\033[0;37m%s", prefix, msg, suffix);
 }
 
-void handle_sdl_error() {
+void print_sdl_error() {
     const char* error = SDL_GetError();
     print_err("SDL2: ", error, "\n");
+}
+
+void handle_sdl_error() {
+    print_sdl_error();
+    exit(1);
 }
 
 int main(int argc, char** argv) {
     int status = SDL_Init(SDL_INIT_VIDEO);
     if (status != 0) {
         handle_sdl_error();
-        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("gwa", 
+                                        SDL_WINDOWPOS_CENTERED, 
+                                        SDL_WINDOWPOS_CENTERED, 
+                                        640, 480, SDL_WINDOW_OPENGL);
+    if (win == NULL) {
+        handle_sdl_error();
+    }
+
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (rend == NULL) {
+        handle_sdl_error();
+    }
+
+    SDL_Event event;
+    bool is_running = true;
+
+    while (is_running) {
+        while (SDL_PollEvent(&event) == 1) {
+            if (event.type == SDL_QUIT) {
+                is_running = false;
+            }
+        }
+
+        status = SDL_SetRenderDrawColor(rend, 128, 128, 128, 255);
+        if (status != 0) {
+            handle_sdl_error();
+        }
+
+        status = SDL_RenderClear(rend);
+        if (status != 0) {
+            handle_sdl_error();
+        }
+
+        SDL_RenderPresent(rend);
     }
 
     SDL_Quit();
