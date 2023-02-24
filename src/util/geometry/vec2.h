65707f75a9e445cdc27da8c5ace8c48089d6cf50
magley <60552482+magley@users.noreply.github.com>
2023-02-24 22:42:37 +0100

    feat: Add vec2

---
+++ src/util/geometry/vec2.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "util/fixed/fp.h"
+
+struct vec2 {
+    fp6 x, y;
+
+    vec2(fp6 x, fp6 y);
+    vec2(const vec2& v);
+    vec2();
+
+    bool operator==(const vec2& v) const;
+    bool operator!=(const vec2& v) const;
+    vec2& operator=(const vec2& v);
+    vec2 operator+(const vec2& v) const;
+    vec2 operator-(const vec2& v) const;
+    vec2 operator*(const fp6& v) const;
+    vec2 operator/(const fp6& v) const;
+    vec2& operator+=(const vec2& v);
+    vec2& operator-=(const vec2& v);
+    vec2& operator*=(const fp6& v);
+    vec2& operator/=(const fp6& v);
+    vec2 operator+() const;
+    vec2 operator-() const;
+};
+
+vec2 operator*(const fp6& f, const vec2& v);
